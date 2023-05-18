38c7b56deb2289ed23f1b78de1e9bbeded898284
daBeeDev <132164375+daBeeDev@users.noreply.github.com>
2023-05-18 17:22:59 +0100

    Update eggGetter.lua

--- eggGetter.lua
+++ eggGetter.lua
@@ -1,3 +1,8 @@
+function getFuel()
+
+end
+
+
 function rturn()
     turtle.turnRight()
     turtle.forward()
@@ -37,52 +42,49 @@ end
 
 function dropEggs()
     for i = 1,5 do
-    x = turtle.getSelectedSlot()
-    turtle.drop()
-    turtle.select(x+1)
-end
-
+    	x = turtle.getSelectedSlot()
+    	turtle.drop()
+    	turtle.select(x+1)
+	end
 end
 
 
 function fuells()
     fuelle = turtle.getFuelLevel()
-    if fuelle < 100 then
-    turtle.select(16)
-
-    turtle.refuel()
-
-end 
-
+	if fuelle < 100 then
+    	turtle.select(16)
+    	turtsle.refuel()
+	end 
 end
 
 
 print("How many eggs?")
-
-
 eggsInput = read()
-
-rawStacks = eggsInput/16  
-
+eggs = tonumber(eggsInput)
+rawStacks = eggs/16  
 stacks = math.floor(rawStacks)
-
 print(stacks)
+    if stacks>4 then
+		rawTrips = eggs/64
+		trips = math.ceil(rawTrips)
+    else
+    	trips = 1
+end
 
-
-fuells()
-
-turtle.select(1)
-
-turtle.suck(16*stacks)
-
-turtle.back()
-
-up()
-
-dropEggs()
-
-down()
-
-turtle.forward()
-
-turtle.select(1)
+for i = 1, trips do
+	fuells()
+		if eggs > 64 then
+			turtle.select(1)
+			turtle.suck(64)
+			eggs = eggs - 64
+		else
+			turtle.select(1)
+			turtle.suck(eggs)
+		end
+	turtle.back()
+	up()
+	dropEggs()
+	down()
+	turtle.forward()
+	turtle.select(1)
+end
