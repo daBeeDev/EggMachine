822c68b6c4a8563f8736614803627ce6a02e666e
daBeeDev <132164375+daBeeDev@users.noreply.github.com>
2023-05-19 21:23:32 +0100

    Update eggLauncher.lua

--- eggLauncher/eggLauncher.lua
+++ eggLauncher/eggLauncher.lua
@@ -1,62 +1,42 @@
-function rturn()
-    turtle.turnRight()
-    turtle.forward()
-    turtle.turnLeft()
-    turtle.forward()
-    turtle.forward()
+function launch()
+    redstone.setOutput('right' , true)
+    sleep(0.1)
+    redstone.setOutput('left', true)
+    sleep(0.1)
+    redstone.setOutput('right', false)
+    sleep(0.1)
+    redstone.setOutput('left' , false)
 end
 
-function leave()
-    turtle.back()
-    turtle.turnLeft()
-    turtle.forward()
-    turtle.turnRight()
+function getInput()
+    term.setCursorPos(1,2)
+    term.write('LaunchEggs!')
+    sleep(1)
+    term.clear()
+    term.setCursorPos(1,2)
+    term.write("how many eggs?")
+    eggs = read()
+    x = eggs / 2
 end
 
-function up()
-    for i = 1, 7 do    
-        turtle.up()
-    end
-end 
-
-function down()
-    for i = 1 ,7 do
-        turtle.down()
-    end
+term.clear()
+perc = 4
+progress = 1 
+getInput()
+term.clear()
+for i=1, x do
+    launch()
+    perc = 0
+    progresstemp = progress/eggs 
+    term.setCursorPos(25,3)
+    term.clearLine()
+    term.write(progresstemp * 200 )
+    perc = progresstemp * 100
+    term.setCursorPos(perc,2)
+    term.write('o')
+    progress = progress + 1   
+    sleep(0.1)
 end
 
-function slotCheck()
-end
-
-function dropEggs()
-    for i = 1,5 do
-    x = turtle.getSelectedSlot()
-    turtle.drop()
-    turtle.select(x+1)
-end
-end
-
-function fuells()
-    fuelle = turtle.getFuelLevel()
-    if fuelle < 100 then
-    turtle.select(16)
-    turtle.refuel()
-end 
-end
-
-print("How many eggs?")
-
-eggsInput = read()
-rawStacks = eggsInput/16  
-stacks = math.floor(rawStacks)
-print(stacks)
-
-fuells()
-turtle.select(1)
-turtle.suck(16*stacks)
-turtle.back()
-up()
-dropEggs()
-down()
-turtle.forward()
+term.setCursorPos(1,4)
 turtle.select(1)
