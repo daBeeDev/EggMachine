2ea6c432aa4ed15ee621fdf7ca587a1fb612652d
daBeeDev <132164375+daBeeDev@users.noreply.github.com>
2023-05-20 20:02:19 +0100

    Update eggLauncher.lua

--- eggLauncher/eggLauncher.lua
+++ eggLauncher/eggLauncher.lua
@@ -5,7 +5,7 @@ function launch()
     sleep(0.1)
     redstone.setOutput('right', false)
     sleep(0.1)
-    redstone.setOutput('left' , false)
+    redstone.setOutput('left' , false)    
 end
 
 function getInput()
@@ -13,8 +13,9 @@ function getInput()
     term.write('LaunchEggs!')
     sleep(1)
     term.clear()
-    term.setCursorPos(1,2)
+    term.setCursorPos(1,3)
     term.write("how many eggs?")
+    term.setCursorPos(2,4)
     eggs = read()
     x = eggs / 2
 end
@@ -30,13 +31,12 @@ for i=1, x do
     progresstemp = progress/eggs 
     term.setCursorPos(25,3)
     term.clearLine()
-    term.write(progresstemp * 200 )
+    percentage = progresstemp * 200
+    term.write(math.floor(percentage)) 
     perc = progresstemp * 100
     term.setCursorPos(perc,2)
     term.write('o')
     progress = progress + 1   
     sleep(0.1)
 end
-
 term.setCursorPos(1,4)
-turtle.select(1)
